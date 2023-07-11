5b33fc7647e24c51a7742817fad584f987c8ebd5
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2023-07-11 12:26:14 -0700

    Creating endpoints for employees in the index

--- index.js
+++ index.js
@@ -6,6 +6,22 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/employees', (req, res) => {
+  res.send('Employees list')
+})
+
+app.post('/employees', (req, res) => {
+  res.send('Create employee')
+})
+
+app.put('/employees/:id', (req, res) => {
+  res.send('Update employee')
+})
+
+app.delete('/employees/:id', (req, res) => {
+  res.send('Delete employee')
+})
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Runing app at http://localhost:${port}`)
 })
