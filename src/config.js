5e81a2bbe21260e1516ca66629c012372f2acc60
Jazziel Puente <60794855+jazzielp@users.noreply.github.com>
2023-07-14 10:03:22 -0700

    Removing the console log in config.js

--- src/config.js
+++ src/config.js
@@ -8,11 +8,4 @@ const DB_PASSWORD = process.env.DB_PASSWORD || 'root'
 const DB_NAME = process.env.DB_NAME || 'employees'
 const DB_PORT = parseInt(process.env.DB_PORT) || 3306
 
-console.log(PORT)
-console.log(DB_HOST)
-console.log(DB_USER)
-console.log(DB_PASSWORD)
-console.log(DB_NAME)
-console.log(DB_PORT)
-
 module.exports = { PORT, DB_PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME }
